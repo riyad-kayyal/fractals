9be2d294f53b32cf44068d31b196ea0b69d13c47
riyad-kayyal <63916794+riyad-kayyal@users.noreply.github.com>
2024-11-11 17:37:11 +0300

    I

--- data/index.ts
+++ data/index.ts
@@ -229,7 +229,7 @@ export const gridItems = [
         id: 1,
         title: "I prioritize client collaboration, fostering open communication ",
         description: "",
-        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        className: "lg:col-span-2 md:col-span-2 md:row-span-2 lg:min-h-[40vh]",
         imgClassName: "w-full h-full",
         titleClassName: "justify-end",
         img: "/b1.svg",
